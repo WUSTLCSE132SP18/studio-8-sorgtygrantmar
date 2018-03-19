3a2347b6356b2342aec2d4bbd908efa4db3aec30
gmarcketta <gmarcketta@URB218-22.seasad.wustl.edu>
2018-03-19 14:23:36 -0500

    yee yee 3/19/18

--- src/studio8/CountSteps.java
+++ src/studio8/CountSteps.java
@@ -5,3 +5,38 @@ package studio8;
 //       1. Create a class and main method.
 //       2. Using a Scanner and File object, read data from your .csv file.
 //       3. Develop and test algorithms to count the "peaks" in the data.
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class CountSteps {
+	
+	public static void main(String[] args) {
+		
+		String countSteps = "H:\\Desktop\\accvalues.csv";
+		BufferedReader br = null;
+		String line = "";
+		String splitBy = ",";
+		
+		try {
+			br = new BufferedReader (new FileReader(countSteps));
+			while((line = br.readLine()) != null) {
+				String[] values = line.split(splitBy);
+				
+				System.out.println( values[0] + " " + values[1] + " " + values[2]);
+			}
+		}
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} 	
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+}
---
+++ steps/steps.ino
@@ -0,0 +1,36 @@
+#include <Wire.h> // Must include Wire library for I2C
+#include <SparkFun_MMA8452Q.h> // Includes the SFE_MMA8452Q library
+
+MMA8452Q accel;
+
+void setup(){
+  Serial.begin(9600);
+  accel.init();
+}
+
+void loop(){
+ if (accel.available()){
+   accel.read();
+   printCalculatedAccels();
+   Serial.println();
+ }
+}
+
+void printAccels() {
+  Serial.print(accel.x, 3);
+  Serial.print("\t");
+  Serial.print(accel.y, 3);
+  Serial.print("\t");
+  Serial.print(accel.z, 3);
+  Serial.print("\t");
+}
+
+void printCalculatedAccels()
+{ 
+  Serial.print(accel.cx, 3);
+  Serial.print(",");
+  Serial.print(accel.cy, 3);
+  Serial.print(",");
+  Serial.print(accel.cz, 3);
+}
+
